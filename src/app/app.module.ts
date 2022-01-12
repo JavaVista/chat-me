c03b7dc200eb83df1637cd6ec027279c83c8835c
Javi Carrion <code-vista@outlook.com>
2022-01-11 23:01:43 -0500

    feat: :sparkles: add components, firebase packages and services in a file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatFormComponent } from './components/chat-form/chat-form.component';
+import { ChatRoomComponent } from './components/chat-room/chat-room.component';
+import { ChatFeedComponent } from './components/chat-feed/chat-feed.component';
+import { MsgComponent } from './components/msg/msg.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { NavComponent } from './components/nav/nav.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserItemComponent } from './components/user-item/user-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatFormComponent,
+    ChatRoomComponent,
+    ChatFeedComponent,
+    MsgComponent,
+    LoginComponent,
+    SignupComponent,
+    NavComponent,
+    UserListComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule,
